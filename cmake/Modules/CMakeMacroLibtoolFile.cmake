e95da3cb726ccae4873d6b1a388853fa193cc971
Pierre Ossman <ossman@cendio.se>
2023-03-27 20:19:55 +0200

    Revert "Fall back to dynamic libraries in .la files"
    This reverses commit e78ebbcb19724491e9ad69ce9bf463161128678f. It is
    incorrect that the linker will fall back on dynamic libraries. It will
    do the opposite, though.

--- cmake/Modules/CMakeMacroLibtoolFile.cmake
+++ cmake/Modules/CMakeMacroLibtoolFile.cmake
@@ -105,19 +105,15 @@ function(libtool_generate_control_file _target)
           endif()
 
           if(STATIC_MODE)
-            set(_library ${CMAKE_STATIC_LIBRARY_PREFIX}${library}${CMAKE_STATIC_LIBRARY_SUFFIX})
-            find_library(FL ${_library} PATHS ${LIBRARY_PATHS})
+            set(library ${CMAKE_STATIC_LIBRARY_PREFIX}${library}${CMAKE_STATIC_LIBRARY_SUFFIX})
           endif()
 
-          if(NOT FL)
-            find_library(FL ${library} PATHS ${LIBRARY_PATHS})
-          endif()
-
+          find_library(FL ${library} PATHS ${LIBRARY_PATHS})
           if(FL)
             # Found library. Depending on if it's static or not we might
             # extract the path and library name, then add the
             # result to the libtool dependency libs.
-            if("${FL}" MATCHES ".+${CMAKE_STATIC_LIBRARY_SUFFIX}$")
+            if(STATIC_MODE)
               set(_target_dependency_libs "${_target_dependency_libs} ${FL}")
             else()
               get_filename_component(_shared_lib ${FL} NAME_WE)
